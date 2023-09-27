6139630cfa9eb58af1a374a0f806b3d5beac6c19
ekatsevi <katsevich.gene@gmail.com>
2023-09-27 19:25:13 -0400

    Fixed issue in submission script

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -2,6 +2,7 @@
 
 #$ -N my_simulation             # Specify job name
 #$ -j y                         # Merge standard output and standard error
+#$ -o logs                      # Specify log directory
 #$ -l h_rt=01:00:00             # Request 1 hour of runtime
 #$ -l h_vmem=1G                 # Request 1 GB of virtual memory per slot
 #$ -t 1-25                      # Specify the task range for the array job
@@ -10,7 +11,7 @@
 module load R/4.3.1
 
 # Calculate parameter setting for this task via scheduler's $SGE_TASK_ID
-parameter_value=$SGE_TASK_ID
+degrees_of_freedom=$SGE_TASK_ID
 
 # Run the simulation with the specific parameter value
-Rscript run_sim.R $param
+Rscript run_sim.R $degrees_of_freedom
